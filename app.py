8e90b733a98fc39adb986f59405704e6118e5068
Rajaie <rajaie.alkorani@gmail.com>
2016-11-30 13:34:47 -0800

    TOTP implemented

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
 from hashlib import sha256
+
+from authy.api import AuthyApiClient
+authy_api = AuthyApiClient('MVZQaKGv2n8k94T377n6bamoVpn3TGEB')
 
 #TODO
 #input validation & sanitization
@@ -10,41 +13,63 @@ from hashlib import sha256
 #   server-side validation
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://human:password@localhost/jadopado'
+app.secret_key = 'Ajoj(*039483jlkjer093#$J#4j343'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://human:password@localhost/jadopado'
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
+########## MODELS ###############
+
 class User(db.Model):
     __tablename__ = "users"
-    email = db.Column(db.String(130), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     name = db.Column(db.String(100))
     password = db.Column(db.LargeBinary())
+    country_code = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    authy_id = db.Column(db.Integer)
 
-    def __init__(self, email, name, password):
-        self.email = email;
-        self.name = name;
-        self.password = password;
+    def __init__(self, email, name, password, country_code, phone, authy_id):
+        self.email = email
+        self.name = name
+        self.password = password
+        self.country_code = country_code
+        self.phone = phone
+        self.authy_id = authy_id
+
+########## UTILS ###############
 
 def hashpw(password):
     return sha256(password.encode('ascii')).hexdigest()
 
+def is_token_formatted(token):
+    return True
+
+########## VIEWS ###############
+
 # Index
 @app.route('/')
-def hello_world():
-    return 'How did you end up here?'
+def index():
+    return render_template('index.html')
 
-# Sign up page
+# Signup
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         email = request.form['email']
         name = request.form['name']
         password = request.form['password']
-        passwordHash = hashpw(password)
+        password_hash = hashpw(password)
+        country_code = request.form['country_code']
+        phone = request.form['phone']
 
-        newUser = User(email, name, passwordHash)
+        authyUser = authy_api.users.create(email, phone, country_code)
+        if not authyUser.ok():
+            return "Authy user creation failed"
+        newUser = User(email, name, password_hash, country_code, phone, authyUser.id)
 
-        # Only add users with unique email
+        # Email address must be unique
         if not User.query.filter_by(email=email).count():
             db.session.add(newUser)
             db.session.commit()
@@ -54,7 +79,7 @@ def signup():
 
     return render_template('signup.html')
 
-# Login page
+# Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -62,10 +87,50 @@ def login():
         password = request.form['password']
 
         user = User.query.filter_by(email=email).first()
+        if user is None:
+            message = "Email doesn't exist, <a href='"+\
+                      url_for('login') + "'>try again</a>"
+            return message
 
-        passwordHash = hashpw(password)
-        if passwordHash == user.password:
-            return "Login successful"
+        password_hash = hashpw(password)
+        if password_hash == user.password:
+            session['authy_id'] = user.authy_id
+            return redirect(url_for('totp'))
         else:
-            return "Wrong email/password combination"
+            message = "Incorrect password, <a href='"+\
+                      url_for('login') + "'>try again</a>"
+            return message
     return render_template('login.html')
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.pop('authy_id', None)
+    return redirect(url_for('index'))
+
+# Authy TOTP
+@app.route('/totp', methods=['GET', 'POST'])
+def totp():
+    authy_id = session.get('authy_id', None)
+
+    if request.method == 'POST':
+        token =  request.form['token']
+
+        # Token not formatted properly
+        if not is_token_formatted(token):
+            message = "Bad token format, <a href='" + \
+                       url_for('totp') + "'>try again</a>"
+            return message
+
+        # Send token verification request to Authy
+        verification = authy_api.tokens.verify(authy_id, token)
+        if verification.ok():
+            session['logged_in'] = True
+            return "Token verified. Login complete."
+        else:
+            message = "Incorrect token, <a href='" + \
+                       url_for('totp') + "'>try again</a>"
+            return message
+
+    return render_template('totp.html')
